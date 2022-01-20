88a15265afd4df3c7266be9a6f369419e6cd14b2
Evgeny Regentovskiy <evgeny.regent@gmail.com>
2022-01-20 18:26:30 +0300

    Init commit

--- less1/index.php
+++ less1/index.php
@@ -3,7 +3,7 @@ require_once $_SERVER["DOCUMENT_ROOT"] . '/bitrix/header.php';
 
 $APPLICATION->SetTitle('Новости');
 
-?><?php $APPLICATION->IncludeComponent("bitrix:news.list", "cardslist", [
+?><?php $APPLICATION->IncludeComponent("bitrix:news.list", "link", [
     "IBLOCK_TYPE" => "cards",
     "IBLOCK_ID" => "8",
     "NEWS_COUNT" => "5",
